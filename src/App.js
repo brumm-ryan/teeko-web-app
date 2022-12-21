097f7ec2cc27917698b43b9f768422d886083dff
brumm-ryan <71672558+brumm-ryan@users.noreply.github.com>
2022-12-20 23:19:13 -0600

    Implemented the front end of teeko game.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BoardTile from "./BoardTile";
+import "./BoardTile.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * The main application component.
+ *
+ */
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        board: [[0,0,0,0,0], [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]], // Our game board
+        aiPieces:[],
+        playerPieces:[],
+        inProgress:false // is the game in progress
+    };
+    this.playerChooseTile = this.playerChooseTile.bind(this)
+  }
+
+  getMoveFromAI() {
+      //TODO implement python api using flask to return ai move
+      let stringBoard = `${this.state.board.flat()}`
+      let response = fetch(`http://pythonAI/${stringBoard}`).then((response) => response.json())
+      return response['move']
+  }
+
+  playerChooseTile(player, xCord, yCord) {
+      let tempBoard = this.state.board.map(function(arr) {
+          return arr.slice();
+      });
+      tempBoard[xCord][yCord] = player;
+      if(player === 1) {
+        if(this.state.playerPieces.length > 3) {
+            let removePiece = this.state.playerPieces.shift()
+            tempBoard[removePiece[0]][removePiece[1]] = 0
+        }
+          this.state.playerPieces.push([xCord, yCord])
+          this.setState({board: tempBoard});
+          //let aiMove = this.getMoveFromAI();
+          //this.playerChooseTile(2, aiMove[0], aiMove[1]);
+      } else {
+          if (player === 2) {
+              if(this.state.aiPieces.length > 3) {
+                  let removePiece = this.state.aiPieces.shift()
+                  tempBoard[removePiece[0]][removePiece[1]] = 0
+              }
+              this.state.aiPieces.push([xCord, yCord])
+              this.setState({board: tempBoard});
+          }
+      }
+  }
+
+  generateBoard() {
+    if(this.state.inProgress) {
+        let boardTiles = []
+            console.log("building board");
+        console.log(this.state.board);
+            this.state.board.forEach((row, x) => {
+            row.forEach((tile, y) => {
+                boardTiles.push(<BoardTile key={`${x},${y}`} xAxis={x} yAxis={y} chooseTileCallback={this.playerChooseTile}
+                                           currentVal={this.state.board[x][y]}>tile</BoardTile>);
+            })
+        })
+        return (<div className="grid-container">{boardTiles}</div>)
+    }
+  }
+
+  render() {
+    return (
+        <div style={{textAlign:"center"}}>
+          <h1>Can you beat this AI at Teeko?</h1>
+          <button hidden={this.state.inProgress} onClick={() => this.setState({inProgress:true})}>I'll try</button>
+          <div>{this.generateBoard()}</div>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/BoardTile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./BoardTile.css";
+
+/**
+ * A piece representing one tile of the Teeko board grid
+ *
+ */
+class BoardTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPlaced: this.props.currentVal !== 0,
+            player: this.props.player,
+            color: "#D3D3D3"
+        };
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.currentVal === 1 && this.state.player !== 1) {
+            console.log("Should be blue anymore")
+            this.setState({isPlaced:true, player:1, color:"#0000FF"})
+        } else if(this.props.currentVal === 2 && this.state.player !== 2) {
+            this.setState({isPlaced:true, player:2, color:"red"})
+        } else if(this.props.currentVal === 0 && this.state.player !== 0){
+            this.setState({isPlaced:false, player:0, color:"#D3D3D3"})
+        }
+    }
+
+    chooseTile() {
+        if(!this.state.isPlaced) {
+            this.props.chooseTileCallback(1, this.props.xAxis, this.props.yAxis)
+            this.setState({isPlaced:true, player:1, color:"#0000FF"})
+            console.log(`x-Axis:${this.props.xAxis} y-Axis:${this.props.yAxis}`)
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className="grid-item">
+                    <div className="color-box" style={{backgroundColor: this.state.color, width:'5em', height:'5em'}}
+                         onClick={() => this.chooseTile()}></div>
+                </div>
+            </>
+
+        );
+    }
+}
+
+export default BoardTile;
